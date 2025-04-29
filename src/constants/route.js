4a1ccc8f4b2401c9913af278989bcb18841741c7
umgeunchan <onionmat1@gmail.com>
2025-04-29 22:38:23 +0900

    FAQ, 포인트, 봉사내역 페이지, 페이지 번호, 포인트 게이지 추가 & 푸터,버튼 병합문제 해결

---
+++ src/constants/route.js
@@ -0,0 +1,13 @@
+const routes = {
+    main: "/",
+    start: "/start",
+    login: "/login",
+    signup: "/signup",
+    point: "/point",
+    myinfo: "/myinfo",
+    faq: "/faq",
+    history: "/history",
+    
+}
+
+export default routes;
--- src/constants/routes.js
+++ src/constants/routes.js
@@ -3,6 +3,10 @@ const routes = {
     start: "/start",
     login: "/login",
     signup: "/signup",
+    point: "/point",
+    myinfo: "/myinfo",
+    faq: "/faq",
+    history: "/history",
     
 }
 
