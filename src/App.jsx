3725c3322fd10d5f8a58050a1fd07f669bab236a
poby123 <i2pir@naver.com>
2025-05-06 12:09:13 +0900

    feat: react-query init

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import Header from './components/Header/Header';
-import BottomNavigation from './components/BottomNavigation/BottomNavigation';
 import { Outlet } from 'react-router-dom';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './App.css';
 import './styles/global.css';
 
+const queryClient = new QueryClient();
+
 function App() {
-  return <Outlet />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Outlet />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
