7528cbaaafd5d49cc585ca0c95fd8cab424ad6df
DongJae Kim <ehdwo1227@naver.com>
2025-05-25 20:23:41 +0900

    f: volunteer map pagenation

--- src/features/volunteer-work/api/api.js
+++ src/features/volunteer-work/api/api.js
@@ -4,8 +4,9 @@ import * as Model from './model';
 /**
  * 봉사활동 목록을 가져옵니다.
  * @typedef {Object} GetVolunteerWorkRequest
- * @property {number} page 페이지
- * @property {number} size 크기
+ * @property {string} latitude 위도 - 기본값: 숭실대 위치 
+ * @property {string} longitude 경도 - 기본값: 숭실대 위치
+ * @property {number} distanceKm 범위 - 기본값: 10
  * @property {string} keyword 검색어
  *
  * @param {GetVolunteerWorkRequest} request
@@ -18,28 +19,28 @@ export const getVolunteerWorkList = async (request) => {
     },
   });
 
-  return data.result;
+  return data;
 };
 
 /**
  * 봉사활동 목록을 가져옵니다.
- * @typedef {Object} GetVolunteerWorkRequestByGeometry
- * @property {number} latitude 위도
- * @property {number} longitude 경도
- * @property {number} distanceKm 범위 - 기본값: 5km
- *
- * @param {GetVolunteerWorkRequestByGeometry} request
- * @returns {Promise<Model.VolunteerWork[]>}
- */
-export const getVolunteerWorkListByGeometry = async (request) => {
-  const { data } = await publicAxios.get('/volunteer-work/geometry', {
-    params: {
-      ...request,
-    },
-  });
+//  * @typedef {Object} GetVolunteerWorkRequestByGeometry
+//  * @property {number} latitude 위도
+//  * @property {number} longitude 경도
+//  * @property {number} distanceKm 범위 - 기본값: 5km
+//  *
+//  * @param {GetVolunteerWorkRequestByGeometry} request
+//  * @returns {Promise<Model.VolunteerWork[]>}
+//  */
+// export const getVolunteerWorkListByGeometry = async (request) => {
+//   const { data } = await publicAxios.get('/volunteer-work/geometry', {
+//     params: {
+//       ...request,
+//     },
+//   });
 
-  return data.result;
-};
+//   return data.result;
+// };
 
 /**
  * 봉사활동 상세를 가져옵니다.
